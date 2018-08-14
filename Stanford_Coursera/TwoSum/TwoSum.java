444b54bee79f733843fa80da5907dad035d5ffea
Chi <31902846+Flotter77@users.noreply.github.com>
2018-08-14 00:58:46 -0400

    Update TwoSum.java

--- Stanford_Coursera/TwoSum/TwoSum.java
+++ Stanford_Coursera/TwoSum/TwoSum.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 /* Inspired by @HuangLiPang Two-Sum_Algorithm(hash table chaining).c */
 
+
 public class TwoSum {
 
     public static int idx(Long value){
@@ -17,34 +18,34 @@ public class TwoSum {
 
     public static void main(String args[]) {
         /*Separate input of long variable into positive and negative hashmaps*/
-        myHashMap phashmap = new myHashMap();
-        myHashMap nhashmap = new myHashMap();
+        myHashTable phashtable = new myHashTable();
+        myHashTable nhashtable = new myHashTable();
         while (StdIn.hasNextLine()) {
             Long input = StdIn.readLong();
             if (input < 0) {
-                nhashmap.put(input);
+                nhashtable.put(input);
               //  System.out.println("index: " + idx(input) + "nhead.val: " + nhashmap.buckets[idx(input)].head.next.val);
             } else {
-                phashmap.put(input);
+                phashtable.put(input);
               //  System.out.println("index: " + idx(input) + "phead.val: " + phashmap.buckets[idx(input)].head.next.val);
             }
         }
-        System.out.println("Negative numbers size " + nhashmap.size() + "; " + "Positive numbers size " + phashmap.size());
+        System.out.println("Negative numbers size " + nhashtable.size() + "; " + "Positive numbers size " + phashtable.size());
 
         /*Store the result in a hashset*/
         HashSet<Long> result = new HashSet<>();
 
 
-        for (Long val : phashmap.keySet()) {
+        for (Long val : phashtable.keySet()) {
             Long tempValue = (val - (val % 10000)) * (-1);
         //    System.out.println("Temp value: " + tempValue);
             /* so only iterate 3 times */
             for (int i = -10000; i <= 10000; i += 10000) {
                 int index = idx(tempValue + i);
                 if (tempValue < 0) {
-                    if (nhashmap.contains(index)) {
+                    if (nhashtable.contains(index)) {
                       //  System.out.println("nIndex:" + index);
-                        ListNode nodePtr = nhashmap.buckets[index].head, prev = null;
+                        ListNode nodePtr = nhashtable.buckets[index].head, prev = null;
                         while (nodePtr.next != null) {
                             prev = nodePtr;
                             nodePtr = nodePtr.next;
@@ -57,9 +58,9 @@ public class TwoSum {
                         }
                     }
                 } else {
-                    if (phashmap.contains(index)) {
+                    if (phashtable.contains(index)) {
                       //  System.out.println("pIndex:" + index);
-                        ListNode nodePtr = phashmap.buckets[index].head, prev = null;
+                        ListNode nodePtr = phashtable.buckets[index].head, prev = null;
                         while (nodePtr.next != null) {
                             prev = nodePtr;
                             nodePtr = nodePtr.next;
