d7c64a13c17e51d066d89134bbae0ebb1ff468f6
Chi <31902846+Flotter77@users.noreply.github.com>
2020-02-04 23:28:44 -0500

    Add files via upload

---
+++ compression/BWtest.java
@@ -0,0 +1,40 @@
+/* *****************************************************************************
+ *  Name:    Alan Turing
+ *  NetID:   aturing
+ *  Precept: P00
+ *
+ *  Partner Name:    Ada Lovelace
+ *  Partner NetID:   alovelace
+ *  Partner Precept: P00
+ *
+ *  Description:  Prints 'Hello, World' to the terminal window.
+ *                By tradition, this is everyone's first program.
+ *                Prof. Brian Kernighan initiated this tradition in 1974.
+ *
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Huffman;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class BWtest {
+    public static void main(String[] args) {
+        // compression
+        if (args[0].equals("-")) {
+            Stopwatch stopwatch = new Stopwatch();
+            BurrowsWheeler.transform();
+            MoveToFront.encode();
+            Huffman.compress();
+            double time = stopwatch.elapsedTime();
+            StdOut.println("The compression time is " + time);
+        }
+        else if (args[0].equals("+")) {
+            Stopwatch stopwatch = new Stopwatch();
+            Huffman.expand();
+            MoveToFront.decode();
+            BurrowsWheeler.inverseTransform();
+            double time = stopwatch.elapsedTime();
+            StdOut.println("The expand time is " + time);
+        }
+    }
+}
