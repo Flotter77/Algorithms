7273a98a0e8f6dc4c7b106809efea6dadffd31a2
Chi <31902846+Flotter77@users.noreply.github.com>
2020-02-04 23:29:07 -0500

    Update BWtest.java

--- compression/BWtest.java
+++ compression/BWtest.java
@@ -1,11 +1,4 @@
 /* *****************************************************************************
- *  Name:    Alan Turing
- *  NetID:   aturing
- *  Precept: P00
- *
- *  Partner Name:    Ada Lovelace
- *  Partner NetID:   alovelace
- *  Partner Precept: P00
  *
  *  Description:  Prints 'Hello, World' to the terminal window.
  *                By tradition, this is everyone's first program.
